b181f9caaed9d5bc529257dbdd58fe84a15cc656
Alfa Art <alfa76519@gmail.com>
2026-03-09 00:39:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jellyshooterbgs-v3/', // GitHub Pages repo name
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: '/jellyshooterbgs-v3/',
 })
